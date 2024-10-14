3812fcb0957d3b0aa5645dcea944049405e31074
Daniel Heilper <daniel.heilper@honeydew.ai>
2024-10-14 19:40:49 +0300

    make the library stati and add optimization flags for vectorization and profile guided

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
 from Cython.Distutils import build_ext as cython_build_ext
@@ -11,12 +11,19 @@ base_compiler_directives = {
     "language_level": 3,
     "overflowcheck": True,
     "cdivision": True,
-    "cpow": True,
+    # "cpow": True,
     "infer_types": True,
     "embedsignature": True,
     "c_api_binop_methods": True,
     "profile": True,
 }
+
+# add CFLAGS:
+# https://pythonspeed.com/articles/faster-cython-simd/
+
+# https://llvm.org/docs/Vectorizers.html
+
+os.environ["CFLAGS"] = f"-Rpass-analysis=loop-vectorize"
 
 # Get the number of physical cores
 num_cores = multiprocessing.cpu_count()
@@ -29,9 +36,21 @@ class build_ext(cython_build_ext):
         self.parallel = num_cores
 
 
+
+ext_modules = [
+    Extension(
+        "pyparsing",
+        sources=py_files,
+        extra_compile_args=["-pgo"],
+        extra_link_args=["-static"],  # Create a static library
+    )
+]
+
+
 setup(
     name="pyparsing",
     version="3.1.4",
-    ext_modules=extensions,
+    # ext_modules=extensions,
+    ext_modules=cythonize(ext_modules, compiler_directives=base_compiler_directives),
     cmdclass={'build_ext': build_ext},    
 )
