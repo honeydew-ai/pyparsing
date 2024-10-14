d63259189ffd64c1663eb97d2ea0ab94bd8027df
Daniel Heilper <daniel.heilper@honeydew.ai>
2024-10-14 22:44:14 +0300

    more flags

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ from Cython.Distutils import build_ext as cython_build_ext
 import multiprocessing
 
 # This will find all .py files in the pyparsing package
-py_files = [os.path.join("pyparsing", file) for file in os.listdir("pyparsing") if file.endswith(".py")]
+py_files = [
+    os.path.join("pyparsing", file)
+    for file in os.listdir("pyparsing")
+    if file.endswith(".py")
+]
 py_files.append(os.path.join("pyparsing", "diagram", "__init__.py"))
 base_compiler_directives = {
     "language_level": 3,
@@ -26,46 +30,53 @@ base_compiler_directives = {
 # os.environ["CFLAGS"] = f"-Rpass-analysis=loop-vectorize"
 
 # add link time optimization flag:
-os.environ["CFLAGS"] = "-flto"
+os.environ["CFLAGS"] = "-flto=thin -fprofile-generate"
 # Get the number of physical cores
 num_cores = multiprocessing.cpu_count()
-extensions = cythonize(py_files, compiler_directives=base_compiler_directives, nthreads=8)
+extensions = cythonize(
+    py_files, compiler_directives=base_compiler_directives, nthreads=8
+)
+
 
 # Custom build_ext command to include -j option
 class build_ext(cython_build_ext):
-    # def initialize_options(self):
-    #     super().initialize_options()
-    #     self.parallel = num_cores
-    def build_extension(self, ext):
-        # Compile the extension as usual
-        super().build_extension(ext)
-
-        # Get the object files
-        objects = self.compiler.object_filenames(ext.sources, output_dir=self.build_temp)
-        
-        # Create the static library
-        # Create the static library
-        static_lib_path = os.path.join(self.build_lib, f"lib{ext.name}.a")
-        self.compiler.create_static_lib(objects, ext.name, output_dir=self.build_lib)
-        
-        # Remove the shared library
-        shared_lib_path = self.get_ext_fullpath(ext.name)
-        if os.path.exists(shared_lib_path):
-            os.remove(shared_lib_path)
-        
-        # Move the static library to the final destination
-        final_static_lib_path = self.get_ext_fullpath(ext.name).replace('.so', '.a')
-        if os.path.exists(static_lib_path):
-            os.rename(static_lib_path, final_static_lib_path)
+    def initialize_options(self):
+        super().initialize_options()
+        self.parallel = num_cores
 
 
+# class build_ext(cython_build_ext):
+#     # def initialize_options(self):
+#     #     super().initialize_options()
+#     #     self.parallel = num_cores
+#     def build_extension(self, ext):
+#         # Compile the extension as usual
+#         super().build_extension(ext)
+
+#         # Get the object files
+#         objects = self.compiler.object_filenames(ext.sources, output_dir=self.build_temp)
+
+#         # Create the static library
+#         # Create the static library
+#         static_lib_path = os.path.join(self.build_lib, f"lib{ext.name}.a")
+#         self.compiler.create_static_lib(objects, ext.name, output_dir=self.build_lib)
+
+#         # Remove the shared library
+#         shared_lib_path = self.get_ext_fullpath(ext.name)
+#         if os.path.exists(shared_lib_path):
+#             os.remove(shared_lib_path)
+
+#         # Move the static library to the final destination
+#         final_static_lib_path = self.get_ext_fullpath(ext.name).replace('.so', '.a')
+#         if os.path.exists(static_lib_path):
+#             os.rename(static_lib_path, final_static_lib_path)
 
 
 ext_modules = [
     Extension(
         "pyparsing",
         sources=py_files,
-        extra_compile_args=[ "-flto", "-march=native"],
+        extra_compile_args=["-flto", "-march=native"],
         extra_link_args=["-flto"],  # Create a static library
     )
 ]
@@ -76,5 +87,5 @@ setup(
     version="3.1.4",
     # ext_modules=extensions,
     ext_modules=cythonize(ext_modules, compiler_directives=base_compiler_directives),
-    cmdclass={'build_ext': build_ext},    
+    cmdclass={"build_ext": build_ext},
 )
